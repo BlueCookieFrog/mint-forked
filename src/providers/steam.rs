2bfc412fb3d56aa333ff72bb64df18bd47c614ce
Connor J Thompson <ct770a@gmail.com>
2024-10-28 13:52:18 +1100

    mapped ISteamUser

--- src/providers/steam.rs
+++ src/providers/steam.rs
@@ -4,7 +4,16 @@ type HSteamPipe = i32;
 type HSteamUser = u32;
 type HMODULE = u64;
 
+type HAuthTicket = u32;
+
+type CGameID = u64;
+type CSteamID = u64;
+type AppId_t = u32;
 type EAccountType = i32;
+type EVoiceResult = i32;
+type EUserHasLicenseForAppResult = i32;
+type EBeginAuthSessionResult = i32;
+type EDurationControlOnlineState = i32;
 type SteamAPIWarningMessageHook_t = u64;
 type SteamAPI_CheckCallbackRegistered_t = u64; // uhh i dont think this one is right
 
@@ -15,48 +24,39 @@ type UNK_PTR = *mut u32;
 
 #[repr(C)]
 pub struct ISteamUser__bindgen_vtable{
-    wwwwwww: *const extern "C" fn(*mut ISteamUser, wwwwwwww) -> wwwwwwwww,
-
-
-    /*
-virtual HSteamUser GetHSteamUser() = 0;
-virtual bool BLoggedOn() = 0;
-virtual CSteamID GetSteamID() = 0;
-virtual int InitiateGameConnection_DEPRECATED( void *pAuthBlob, int cbMaxAuthBlob, CSteamID steamIDGameServer, uint32 unIPServer, uint16 usPortServer, bool bSecure ) = 0;
-virtual void TerminateGameConnection_DEPRECATED( uint32 unIPServer, uint16 usPortServer ) = 0;
-virtual void TrackAppUsageEvent( CGameID gameID, int eAppUsageEvent, const char *pchExtraInfo = "" ) = 0;
-virtual bool GetUserDataFolder( char *pchBuffer, int cubBuffer ) = 0;
-virtual void StartVoiceRecording( ) = 0;
-virtual void StopVoiceRecording( ) = 0;
-virtual EVoiceResult GetAvailableVoice( uint32 *pcbCompressed, uint32 *pcbUncompressed_Deprecated = 0, uint32 nUncompressedVoiceDesiredSampleRate_Deprecated = 0 ) = 0;
-virtual EVoiceResult GetVoice( bool bWantCompressed, void *pDestBuffer, uint32 cbDestBufferSize, uint32 *nBytesWritten, bool bWantUncompressed_Deprecated = false, void *pUncompressedDestBuffer_Deprecated = 0, uint32 cbUncompressedDestBufferSize_Deprecated = 0, uint32 *nUncompressBytesWritten_Deprecated = 0, uint32 nUncompressedVoiceDesiredSampleRate_Deprecated = 0 ) = 0;
-virtual EVoiceResult DecompressVoice( const void *pCompressed, uint32 cbCompressed, void *pDestBuffer, uint32 cbDestBufferSize, uint32 *nBytesWritten, uint32 nDesiredSampleRate ) = 0;
-virtual uint32 GetVoiceOptimalSampleRate() = 0;
-virtual HAuthTicket GetAuthSessionTicket( void *pTicket, int cbMaxTicket, uint32 *pcbTicket, const SteamNetworkingIdentity *pSteamNetworkingIdentity ) = 0;
-virtual HAuthTicket GetAuthTicketForWebApi( const char *pchIdentity ) = 0;
-virtual EBeginAuthSessionResult BeginAuthSession( const void *pAuthTicket, int cbAuthTicket, CSteamID steamID ) = 0;
-virtual void EndAuthSession( CSteamID steamID ) = 0;
-virtual void CancelAuthTicket( HAuthTicket hAuthTicket ) = 0;
-virtual EUserHasLicenseForAppResult UserHasLicenseForApp( CSteamID steamID, AppId_t appID ) = 0;
-virtual bool BIsBehindNAT() = 0;
-virtual void AdvertiseGame( CSteamID steamIDGameServer, uint32 unIPServer, uint16 usPortServer ) = 0;
-STEAM_CALL_RESULT( EncryptedAppTicketResponse_t )
-virtual SteamAPICall_t RequestEncryptedAppTicket( void *pDataToInclude, int cbDataToInclude ) = 0;
-virtual bool GetEncryptedAppTicket( void *pTicket, int cbMaxTicket, uint32 *pcbTicket ) = 0;
-virtual int GetGameBadgeLevel( int nSeries, bool bFoil ) = 0;
-virtual int GetPlayerSteamLevel() = 0;
-STEAM_CALL_RESULT( StoreAuthURLResponse_t )
-virtual SteamAPICall_t RequestStoreAuthURL( const char *pchRedirectURL ) = 0;
-virtual bool BIsPhoneVerified() = 0;
-virtual bool BIsTwoFactorEnabled() = 0;
-virtual bool BIsPhoneIdentifying() = 0;
-virtual bool BIsPhoneRequiringVerification() = 0;
-STEAM_CALL_RESULT( MarketEligibilityResponse_t )
-virtual SteamAPICall_t GetMarketEligibility() = 0;
-STEAM_CALL_RESULT( DurationControl_t )
-virtual SteamAPICall_t GetDurationControl() = 0;
-virtual bool BSetDurationControlOnlineState( EDurationControlOnlineState eNewState ) = 0;
-     */
+    GetHSteamUser:                  *const extern "C" fn(*mut ISteamUser) -> HSteamUser,
+    BLoggedOn:                      *const extern "C" fn(*mut ISteamUser) -> bool,
+    GetSteamID:                     *const extern "C" fn(*mut ISteamUser) -> CSteamID,
+    InitiateGameConnection_DEPRECATED:  *const extern "C" fn(*mut ISteamUser, UNK_PTR, i32, CSteamID, u32, u16, bool) -> i32, // p2: void *pAuthBlob
+    TerminateGameConnection_DEPRECATED: *const extern "C" fn(*mut ISteamUser, u32, u16),
+    TrackAppUsageEvent:             *const extern "C" fn(*mut ISteamUser, CGameID, i32, *const u8), // p4 is optional??
+    GetUserDataFolder:              *const extern "C" fn(*mut ISteamUser,  *mut u8, i32) -> bool,
+    StartVoiceRecording:            *const extern "C" fn(*mut ISteamUser),
+    StopVoiceRecording:             *const extern "C" fn(*mut ISteamUser),
+    GetAvailableVoice:              *const extern "C" fn(*mut ISteamUser, *mut u32, *mut u32, u32) -> EVoiceResult, // default params 3,4
+    GetVoice:                       *const extern "C" fn(*mut ISteamUser, bool, UNK_PTR, u32, u32, bool, UNK_PTR, u32, *mut u32, u32) -> EVoiceResult,
+    DecompressVoice:                *const extern "C" fn(*mut ISteamUser, UNK_PTR, u32, UNK_PTR, u32, *mut u32, u32) -> EVoiceResult,
+    GetVoiceOptimalSampleRate:      *const extern "C" fn(*mut ISteamUser) -> u32,
+    GetAuthSessionTicket:           *const extern "C" fn(*mut ISteamUser, *mut u8, i32, *mut u32, UNK_PTR) -> HAuthTicket, // p5 : SteamNetworkingIdentity
+    GetAuthTicketForWebApi:         *const extern "C" fn(*mut ISteamUser, *mut u8) -> HAuthTicket,
+    BeginAuthSession:               *const extern "C" fn(*mut ISteamUser, *mut u8, i32, CSteamID) -> EBeginAuthSessionResult,
+    EndAuthSession:                 *const extern "C" fn(*mut ISteamUser, CSteamID),
+    CancelAuthTicket:               *const extern "C" fn(*mut ISteamUser, HAuthTicket),
+    UserHasLicenseForApp:           *const extern "C" fn(*mut ISteamUser, CSteamID, AppId_t) -> EUserHasLicenseForAppResult,
+    BIsBehindNAT:                   *const extern "C" fn(*mut ISteamUser) -> bool,
+    AdvertiseGame:                  *const extern "C" fn(*mut ISteamUser, CSteamID, u32, u16),
+    RequestEncryptedAppTicket:      *const extern "C" fn(*mut ISteamUser, *mut u8, i32) -> SteamAPICall_t,
+    GetEncryptedAppTicket:          *const extern "C" fn(*mut ISteamUser, *mut u8, i32, *mut u32) -> bool,
+    GetGameBadgeLevel:              *const extern "C" fn(*mut ISteamUser, i32, bool) -> i32,
+    GetPlayerSteamLevel:            *const extern "C" fn(*mut ISteamUser) -> i32,
+    RequestStoreAuthURL:            *const extern "C" fn(*mut ISteamUser, *mut u8) -> SteamAPICall_t,
+    BIsPhoneVerified:               *const extern "C" fn(*mut ISteamUser) -> bool,
+    BIsTwoFactorEnabled:            *const extern "C" fn(*mut ISteamUser) -> bool,
+    BIsPhoneIdentifying:            *const extern "C" fn(*mut ISteamUser) -> bool,
+    BIsPhoneRequiringVerification:  *const extern "C" fn(*mut ISteamUser) -> bool,
+    GetMarketEligibility:           *const extern "C" fn(*mut ISteamUser) -> SteamAPICall_t,
+    GetDurationControl:             *const extern "C" fn(*mut ISteamUser) -> SteamAPICall_t,
+    BSetDurationControlOnlineState: *const extern "C" fn(*mut ISteamUser, EDurationControlOnlineState) -> bool,
 }
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
@@ -107,7 +107,7 @@ pub struct ISteamClient__bindgen_vtable{
     GetISteamRemotePlay:            *const extern "C" fn(*mut ISteamClient, HSteamUser, HSteamPipe, *mut u8) -> UNK_PTR, // out: ISteamRemotePlay,
     DestroyAllInterfaces:           *const extern "C" fn(*mut ISteamClient),
 }
-pub unsafe fn SteamAPI_ISteamClient_CreateSteamPipe (_self: *mut ISteamClient) -> HSteamPipe{ return (*(*(*_self).vtable_).CreateSteamPipe)(_self); }
+pub unsafe fn ISteamClient_CreateSteamPipe (_self: *mut ISteamClient) -> HSteamPipe{ return (*(*(*_self).vtable_).CreateSteamPipe)(_self); }
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
 pub struct ISteamClient {
@@ -123,7 +123,7 @@ pub struct steam_data{
     DAT_ISteamClient_ptr: *mut ISteamClient,
     DAT_steam_IPC_pipe: HSteamPipe,
     
-    //DAT_ISteamUser_ptr: *mut ISteamUser,
+    DAT_ISteamUser_ptr: *mut ISteamUser,
     DAT_steamclient_hmodule: HMODULE,
     DAT_steam_alt_IPC_pipe: HSteamPipe,
     DAT_steam_user: HSteamUser
@@ -137,9 +137,11 @@ fn init_steam_client() -> i32{
     // callback();
     return 0;
 }
+fn SteamAPI_Shutdown(){}
 fn init_steam(interfaces: &str) -> i32{
     let steam = steam_data {
         DAT_ISteamClient_ptr: std::ptr::null_mut(),
+        DAT_ISteamUser_ptr: std::ptr::null_mut(),
         DAT_steam_IPC_pipe: 0,
         DAT_steamclient_hmodule: 0,
         DAT_steam_alt_IPC_pipe: 0,
@@ -151,10 +153,8 @@ fn init_steam(interfaces: &str) -> i32{
     let result = init_steam_client();
     if result != 0 {return result;}
 
-    let doodying = (*(*(*steam.DAT_ISteamClient_ptr).vtable_).CreateSteamPipe)(steam.DAT_ISteamClient_ptr);
 
-
-    //steam.DAT_steam_IPC_pipe = (*steam.DAT_ISteamClient_ptr).vtable_.CreateSteamPipe(steam.DAT_ISteamClient_ptr);
+    steam.DAT_steam_IPC_pipe = ISteamClient_CreateSteamPipe(steam.DAT_ISteamClient_ptr);
     if steam.DAT_steam_IPC_pipe == 0 {
         SteamAPI_Shutdown();
         return 2;}
@@ -240,7 +240,6 @@ fn steam_main() -> Result<(), &'static str>{
     11 => return Err("Failed to load steam client module"),
     12 => return Err("Unable to locate interface factory in steamclient64.dll"),
     13 => return Err("failed to load 'SteamClient021' interface"),
-     _ => return Err("invalid steam error"),
      0 => {
         // Set up a background thread to run
         // std::thread HandlerThread = std::thread([&]() {
@@ -261,6 +260,7 @@ fn steam_main() -> Result<(), &'static str>{
         //SteamAuthComplete.get_future().wait();
 
         return Ok(());
-    }}
-    return Ok(());
+    }
+    _ => return Err("invalid steam error")
+    }
 }
