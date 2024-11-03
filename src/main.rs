65b3dd52944c6481e2a5ce42aed9fc124d2fe790
Borys <35380525+BlueCookieFrog@users.noreply.github.com>
2024-11-03 18:21:12 +0100

    Suppress Clippy's type complexity lint

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+#[allow(clippy::type_complexity)]
 
 use std::collections::BTreeSet;
 use std::path::PathBuf;
